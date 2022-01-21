9205f9bbce0d2e0ad8d7a6e9a74f4053c8152ac5
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-21 08:22:06 -0500

    Post Project Tests Passing

--- api/project/model.js
+++ api/project/model.js
@@ -1,8 +1,24 @@
 const db = require('../../data/dbConfig')
 
 
-function getProjects() {
-    return db('projects')
+async function getProjects() {
+    const projects = await db('projects')
+    const response = []
+
+    projects.forEach(p => {
+        const project = {
+            project_id: p.project_id,
+            project_name: p.project_name,
+            project_description: p.project_description,
+            project_completed: p.project_completed === 1 ? true : false
+        }
+
+        response.push(project)
+    })
+
+    return response
+
+
 }
 
 async function addProject(project) {
