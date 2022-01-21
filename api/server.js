33fa22c69d24fedd3b72b5c55556cec174bbf4ff
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-21 07:19:16 -0500

    Projects Endpoints Work

--- api/server.js
+++ api/server.js
@@ -2,10 +2,20 @@
 const express = require('express')
 const server = express()
 const knex = require('../knexfile')
+const projectsRouter = require('./project/router')
 
 server.use(express.json())
+server.use('/api/projects', projectsRouter)
+
 server.get('/', (req, res) => {
     res.send('<h2>Jaysons Sprint API</h2>')
 })
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+      message: err.message,
+      stack: err.stack,
+    })
+  })
 
 module.exports = server
