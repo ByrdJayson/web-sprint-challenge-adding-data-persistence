5e3159112025234392c6fa869755b284dee2b8b1
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-21 06:22:08 -0500

    Project Setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,11 @@
 // build your server here and require it from index.js
+const express = require('express')
+const server = express()
+const knex = require('../knexfile')
+
+server.use(express.json())
+server.get('/', (req, res) => {
+    res.send('<h2>Jaysons Sprint API</h2>')
+})
+
+module.exports = server
